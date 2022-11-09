ceea9e89ced025f688b5f77247a7b8eaca28efc7
Raghu Thilak Gudur Ramadas <71406389+thilak1992@users.noreply.github.com>
2022-11-09 08:42:28 +0530

    Second Commit

--- src/com/Bridgelabz/Day06LogicalProblems/PrimeNumber.java
+++ src/com/Bridgelabz/Day06LogicalProblems/PrimeNumber.java
@@ -2,7 +2,7 @@ package com.Bridgelabz.Day06LogicalProblems;
 
 import java.util.Scanner;
 
-public class PrimeNumber {
+public class PrimeNumber {          //
 
     public static void main(String[] args) {
         System.out.println("Enter the number: ");
